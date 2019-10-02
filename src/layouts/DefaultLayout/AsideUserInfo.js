ce5cab2e9668ea730bd6e56e0bf3966476640708
Shridhar TL <shridhar.tl@gmail.com>
2019-10-02 11:02:21 +0530

    #69 - Added current instance of jira server name to header.

--- src/layouts/DefaultLayout/AsideUserInfo.js
+++ src/layouts/DefaultLayout/AsideUserInfo.js
@@ -1,43 +1,25 @@
 import React, { PureComponent } from 'react';
 import { inject } from '../../services';
 import { AppSidebarHeader } from '@coreui/react';
-import { UncontrolledDropdown, DropdownItem, DropdownMenu, DropdownToggle } from 'reactstrap';
-import $ from "jquery";
-import { AppContext } from '../../App';
+import { UncontrolledDropdown, DropdownMenu, DropdownToggle } from 'reactstrap';
+import SwitchAccountMenu from './SwitchAccountMenu';
 
 class AsideUserInfo extends PureComponent {
-    static contextType = AppContext;
-
     constructor(props) {
         super(props);
-        inject(this, "SessionService", "UserService", "UserUtilsService");
+        inject(this, "SessionService");
 
         const { jiraUser: { displayName, emailAddress, key, avatarUrls } } = this.$session.CurrentUser;
         this.state = {
             name: displayName,
             login: key,
-            profileUrl: this.$userutils.getProfileUrl(),
             emailAddress,
             imageUrl: avatarUrls["24x24"]
         };
     }
 
-    UNSAFE_componentWillMount() {
-        this.$user.getUsersList().then(users => this.setState({ users }));
-    }
-
-    getJiraServerName(url) {
-        return new URL(url).host;
-    }
-
-    switchUser = (e) => {
-        const el = $(e.currentTarget);
-        const userId = parseInt(el.attr("user-id"));
-        this.context.switchUser(userId);
-    }
-
     render() {
-        const { name, emailAddress, imageUrl, login, users, profileUrl } = this.state;
+        const { name, emailAddress, imageUrl, login } = this.state;
 
         return (
             <AppSidebarHeader>
@@ -54,30 +36,7 @@ class AsideUserInfo extends PureComponent {
                         </div>
                     </DropdownToggle>
                     <DropdownMenu right>
-                        {users && users.length > 0 && <>
-                            <DropdownItem header tag="div" className="text-center"><strong>Switch Accounts</strong></DropdownItem>
-
-                            {users.map(u => <DropdownItem key={u.id} tag="span" user-id={u.id} onClick={this.switchUser} title={u.email}>
-                                <i className="fa fa-external-link icon-extl" />
-                                <div className="inline">
-                                    <div>{this.getJiraServerName(u.jiraUrl)}</div>
-                                    <div>({u.userId})</div>
-                                </div>
-                            </DropdownItem>)}
-
-                            <DropdownItem tag="a" href="/index.html#/integrate" title="Integrate with new instance of Jira"><i className="fa fa-plug"></i> Integrate</DropdownItem>
-                            {/*<DropdownItem><i className="fa fa-envelope-o"></i> Messages<Badge color="success">42</Badge></DropdownItem>
-                            <DropdownItem><i className="fa fa-tasks"></i> Tasks<Badge color="danger">42</Badge></DropdownItem>
-                            <DropdownItem><i className="fa fa-comments"></i> Comments<Badge color="warning">42</Badge></DropdownItem>*/}
-                        </>
-                        }
-
-                        <DropdownItem header tag="div" className="text-center"><strong>Settings</strong></DropdownItem>
-                        <DropdownItem tag="a" href={profileUrl} target="_blank"><i className="fa fa-user"></i> Profile</DropdownItem>
-                        {/*<DropdownItem><i className="fa fa-wrench"></i> Settings</DropdownItem>*/}
-
-                        {/*<DropdownItem divider />*/}
-                        <DropdownItem onClick={e => this.props.onLogout(e)}><i className="fa fa-lock"></i> Logout</DropdownItem>
+                        <SwitchAccountMenu />
                     </DropdownMenu>
                 </UncontrolledDropdown>
             </AppSidebarHeader>
