89b8b35adc32da660e61cf74eece356eccc400a6
shridhartl <shridhar_tl@ymail.com>
2020-05-17 10:41:58 +0530

    Minor fixes done to add bookmark without pressing enter key after each ticket

--- src/controls/MultiValueText.js
+++ src/controls/MultiValueText.js
@@ -6,15 +6,23 @@ class MultiValueText extends PureComponent {
         this.props.onChange(e.value);
     }
 
+    onBlur = (e) => {
+        const ticket = e.currentTarget.value?.trim().toUpperCase();
+        if (!ticket || ticket.length < 4) { return; }
+        let { props: { value } } = this;
+        value = value || [];
+        if (!value.some(v => v.toUpperCase() === ticket)) {
+            value = [...value, ticket];
+            e.currentTarget.value = "";
+            this.props.onChange(value);
+        }
+    }
+
     render() {
         const { props: { value, placeholder } } = this;
 
-        return <Chips value={value} onChange={this.valueChanged} placeholder={placeholder} />;
+        return <Chips value={value} onChange={this.valueChanged} placeholder={placeholder} onBlur={this.onBlur} separator="," />;
     }
 }
-
-MultiValueText.propTypes = {
-
-};
 
 export default MultiValueText;
