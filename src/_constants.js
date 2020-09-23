ed26426502025d65d0ae49de267ea8fec61014c3
shridhartl <shridhar_tl@ymail.com>
2020-09-23 11:02:54 +0530

    Modified version number to 2.0

--- src/_constants.js
+++ src/_constants.js
@@ -164,7 +164,7 @@ export const FULL_DAY_NAMES = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thur
 
 export const defaultJiraFields = ["issuetype", "summary", "reporter", "priority", "status", "resolution", "created", "updated"];
 
-export const AppVersionNo = 1.10;
+export const AppVersionNo = 2.0;
 
 let AnalyticsTrackingId = "UA-108841109-1"; // This is for public tracking id
 
