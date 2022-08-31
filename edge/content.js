04464d31d9ec330038cc3e057f8c6fbffd25b702
GitHub Web Deploy CI <shridhar.tl@gmail.com>
2022-08-31 09:41:05 +0000

    Updates

--- edge/content.js
+++
@@ -1,8 +0,0 @@
-/* global browser exportFunction */
-function executeJASvc(extnId, request, resolve, reject) {
-    browser.runtime.sendMessage(request)
-        .then((res) => resolve(JSON.stringify(res)), reject);
-}
-
-console.log('JA Communication started');
-exportFunction(executeJASvc, window, { defineAs: '_executeJASvc' });
