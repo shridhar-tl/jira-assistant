975592afecc6c87dbb8e0ade712141b5be0ab848
shridhar89 <shridhar.tl@gmail.com>
2022-08-31 12:09:23 +0530

    App version updated v2.42

--- src/constants/common.js
+++ src/constants/common.js
@@ -9,7 +9,7 @@ export const GoogleOAuth = {
     }
 };
 
-export const AppVersionNo = 2.41;
+export const AppVersionNo = 2.42;
 
 let AnalyticsTrackingId = "G-CJQYE6Q1JQ"; //"UA-108841109-1"; // This is for public tracking id
 
