e6438f771223c6de34f1bc51fe7dd5d7039c9f89
shridhar89 <shridhar.tl@gmail.com>
2022-10-09 17:04:05 +0530

    Removed unnecessary variable export causing background pages to fail

--- src/constants/build-info.js
+++ src/constants/build-info.js
@@ -6,6 +6,4 @@ const isExtnBuild = !isWebBuild && !isAppBuild;
 const isProdBuild = process.env.NODE_ENV === "production";
 const isDevBuild = !isProdBuild;
 
-const isQuickView = document.location.href.indexOf('?quick=true') > -1;
-
-export { isWebBuild, isAppBuild, isExtnBuild, buildMode, isProdBuild, isDevBuild, isQuickView };
+export { isWebBuild, isAppBuild, isExtnBuild, buildMode, isProdBuild, isDevBuild };
