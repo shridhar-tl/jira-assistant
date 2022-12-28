62cc825f9c5db4b4384daac9a409d21d77194b9f
Shridhar TL <shridhar.tl@gmail.com>
2022-12-28 08:42:18 +0530

    #99 - Added option to toggle between full day and working hours in worklog calendar.

--- src/gadgets/Calendar/Calendar.js
+++ src/gadgets/Calendar/Calendar.js
@@ -73,7 +73,7 @@ class Calendar extends BaseGadget {
             this.minTime = this.minTime * 60 * 60;
         }
 
-        this.fullCalendarOpts = this.getCalendarOptions();
+        this.fullCalendarOpts = this.getCalendarOptions(this.state);
 
         //moment = (date) => toMoment(date, this.calendar)
     }
@@ -84,6 +84,7 @@ class Calendar extends BaseGadget {
             {meetingInfo}
             <li>Drag and drop or resize the worklog to edit it</li>
             <li>Right click worklog to see more options</li>
+            <li>You can change the hours grid in calendar by changing the working<br />and display hours in General Settings page</li>
         </ul>);
     }
 
@@ -131,14 +132,14 @@ class Calendar extends BaseGadget {
         ];
     }
 
-    getCalendarOptions() {
+    getCalendarOptions({ fullView }) {
         const {
             startOfDay, endOfDay,
-            startOfDayDisp, endOfDayDisp,
             startOfWeek, workingDays,
             timeFormat
         } = this.CurrentUser;
         const { viewMode } = (this.isGadget ? this.props : this.state.settings);
+        const { startOfDayDisp, endOfDayDisp } = fullView ? { startOfDayDisp: '00:00', endOfDayDisp: '23:59' } : this.CurrentUser;
 
         let firstDay = startOfWeek;
         if (firstDay && firstDay > 0) {
@@ -948,17 +949,25 @@ class Calendar extends BaseGadget {
         this.$config.saveSettings('calendar', settings);
     };
 
+    toggleDisplayHours = () => this.setState(({ fullView }) => {
+        const newState = { fullView: !fullView };
+        this.fullCalendarOpts = this.getCalendarOptions(newState);
+        return newState;
+    });
+
     renderCustomActions() {
         const {
             isGadget,
-            state: { pendingWorklogCount, isLoading, uploading }
+            state: { pendingWorklogCount, isLoading, uploading, fullView, settings: { viewMode } }
         } = this;
-
+        const isGridMode = viewMode === 'timeGridWeek' || viewMode === 'timeGridDay';
         return <>
+            {isGridMode && <Button type="secondary" icon={fullView ? 'fa fa-compress' : 'fa fa-expand'} onClick={this.toggleDisplayHours}
+                title={fullView ? "Click to show only working hours in calendar" : "Click to show full day calendar"} />}
             {!this.isGadget && <>
                 <Button icon="fa fa-arrow-left" onClick={() => this.calendar.getApi().prev()} />
                 <Button icon="fa fa-arrow-right" onClick={() => this.calendar.getApi().next()} />
-                <SelectBox dataset={viewModes} value={this.state.settings.viewMode} valueField="value" displayField="label" placeholder="Select a view mode" onChange={this.viewModeChanged} />
+                <SelectBox dataset={viewModes} value={viewMode} valueField="value" displayField="label" placeholder="Select a view mode" onChange={this.viewModeChanged} />
             </>}
             <span className="info-badge" title={pendingWorklogCount ? `Upload ${pendingWorklogCount} pending worklog(s)` : 'No worklog pending to be uploaded'}>
                 {pendingWorklogCount > 0 && <span className="info btn-warning">{pendingWorklogCount}</span>}
