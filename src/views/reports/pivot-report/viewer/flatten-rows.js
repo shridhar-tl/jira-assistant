cce23854fb83b27d74f464352a5b7371a81ced86
Shridhar TL <shridhar.tl@gmail.com>
2023-10-01 18:40:56 +0530

    Fixes rowSpan issue while using array fields in middle of row

--- src/views/reports/pivot-report/viewer/flatten-rows.js
+++ src/views/reports/pivot-report/viewer/flatten-rows.js
@@ -24,6 +24,7 @@ function handleCell(cell, result) {
     const startRowCount = result.length;
     const curRow = result[startRowCount - 1];
 
+    const curCellIndex = curRow.length;
     curRow.push(cellInfo);
 
     if (subRows?.length) {
@@ -39,5 +40,19 @@ function handleCell(cell, result) {
 
     if (rowSpan > 1) {
         cellInfo.tagProps = { rowSpan };
+
+        if (curCellIndex) {
+            // If row span is added in the middle of row, then it be because of using a field which is an array.
+            // In such case need to set the row span for all the previous cells as well
+            for (let i = curCellIndex - 1; i >= 0; i--) {
+                const curCell = curRow[i];
+
+                if (!curCell.tagProps) {
+                    curCell.tagProps = { rowSpan };
+                } else if (curCell.tagProps.rowSpan < rowSpan) {
+                    curCell.tagProps.rowSpan = rowSpan;
+                }
+            }
+        }
     }
 }
