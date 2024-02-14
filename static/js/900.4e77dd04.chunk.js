1e761173fd7d8bc64f6e410720c6412ae26356a0
GitHub Web Deploy CI <shridhar.tl@gmail.com>
2024-02-14 02:05:19 +0000

    Updates

---
+++ static/js/900.4e77dd04.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkjira_assistant=globalThis.webpackChunkjira_assistant||[]).push([[900],{6900:(s,a,e)=>{e.r(a),e.d(a,{default:()=>c});var t=e(9584),n=e(1720),r=e(7884);class i extends t.PureComponent{render(){return(0,r.jsx)(n.U5,{className:"widget-cntr",isGadget:!1})}}const c=i}}]);
