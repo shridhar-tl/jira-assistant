09bdff876e58651cb7cf338bf5e2c3638063f569
GitHub Web Deploy CI <shridhar.tl@gmail.com>
2025-08-30 04:25:50 +0000

    Updates

---
+++ edge/static/js/523.1f06a899.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkjira_assistant=globalThis.webpackChunkjira_assistant||[]).push([[523],{9523:(s,a,e)=>{e.r(a),e.d(a,{default:()=>l});var n=e(9950),r=e(3816),t=e(4414);class i extends n.PureComponent{render(){return(0,t.jsx)("div",{className:"calendar-view",children:(0,t.jsx)(r.Vv,{isGadget:!1})})}}const l=i}}]);
