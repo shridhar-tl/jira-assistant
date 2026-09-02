bc2914336543e401cfb76864d2d9e898bdc03dd4
GitHub Web Deploy CI <shridhar.tl@gmail.com>
2026-09-02 04:08:54 +0000

    Updates

---
+++ firefox/assets/CalendarPage-C6kPt5jH.js
@@ -0,0 +1,1 @@
+import{t as e}from"./vendor-react-Bz8HQYLN.js";import{t}from"./Calendar-BEq1LO98.js";var n=e();function r(){return(0,n.jsx)(`div`,{className:`calendar-page h-full flex flex-col`,children:(0,n.jsx)(t,{isGadget:!1})})}export{r as default};
